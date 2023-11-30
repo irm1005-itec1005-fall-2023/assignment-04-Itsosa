d9bd890947cca2e19f5844fc3c4cd6ccfd9da88a
Itsosa <osasimonjr@gmail.com>
2023-11-30 15:07:44 -0500

    My to do list :(

--- script.js
+++ script.js
@@ -36,6 +36,60 @@ function inititialise() {
   // Init complete
   console.log("App successfully initialised");
 }
+function addTask() {
+  if (inputBox.value == "") {
+    alert("Please enter a task");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let tempButton = document.createElement("button");
+    tempButton.innerHTML = "x";
+    li.appendChild(tempButton);
+    
+  }
+  inputBox.value = "";
+  saveData();
+}
+function clearAll() {
+  listContainer.innerHTML = "";
+  saveData();
+}
+function clearCompleted() {
+  let completed = document.getElementsByClassName("checked");
+  while (completed.length > 0) {
+    completed[0].parentNode.removeChild(completed[0]);
+  }
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData()
+  }
+  else if (e.target.tagName === "BUTTON") {
+    e.target.parentNode.remove();
+    saveData()
+  }
+}, false);
+
+
+
+function saveData() {
+  localStorage.setItem("listContainer", listContainer.innerHTML);
+}
+
+function loadData() {
+  if (localStorage.getItem("listContainer")) {
+    listContainer.innerHTML = localStorage.getItem("listContainer");
+  }
+}
+
+loadData();
+const todoEmptyState = document.querySelector(".empty-state");
+todoEmptyState.classList.add("hidden");
 
 //
 // Inits & Event Listeners
